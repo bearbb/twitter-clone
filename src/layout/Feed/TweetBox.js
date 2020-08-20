9d4312a86292da8d3adb524895f986e96b505805
bearbb <nguyen.b2uty@gmail.com>
2020-08-20 22:41:59 +0700

    Finished the tweet header and the input (adding photo or things unavaiable for now remember to add it later)

--- src/layout/Feed/TweetBox.js
+++ src/layout/Feed/TweetBox.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import "./TweetBox.css";
+import { Button, Avatar } from "@material-ui/core";
 function TweetBox() {
-  return <div></div>;
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://images-na.ssl-images-amazon.com/images/I/51AG6B5vv7L._AC_SX425_.jpg"></Avatar>
+          <input type="text" placeholder="What're u doing>" />
+          {/* <input type="text" placeholder="place ur img url here" /> */}
+        </div>
+        <Button className="tweetBox__tweetButton">Tweet</Button>
+      </form>
+    </div>
+  );
 }
 
 export default TweetBox;
